c5513819f1bcac01caf4efcea4a6f6a5a3bed3e2
Rufai5 <muhammedrufai@yahoo.com>
2022-10-16 17:50:56 -0700

    task 100-decimal

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar,59);
+
+	return(1);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-0x00. C - Hello, World
+0x03. Shell, init files, variables and expansions
