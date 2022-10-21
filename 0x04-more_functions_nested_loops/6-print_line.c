52c759266319a3ae16ae9c151eb87f44d57c0fa2
Rufai5 <muhammedrufai@yahoo.com>
2022-10-21 12:54:27 -0700

    6-print_line.c6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ *
+ * @n: parameter
+ *
+ * Return:returns nothing
+ */
+
+void print_line(int n)
+
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
