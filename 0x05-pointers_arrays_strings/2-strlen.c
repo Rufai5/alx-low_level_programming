df9b20ca0f8b34b8fea12958cf1b1e2951729f40
Rufai5 <muhammedrufai@yahoo.com>
2022-10-24 06:27:51 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the lengh of a sting
+ * @s: String
+ * Return: integer;
+ */
+
+int _strlen(char *s)
+
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
