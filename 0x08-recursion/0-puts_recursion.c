7a2cc6b1d0872c8b8c3b4509e30af8be039a3b21
Rufai5 <muhammedrufai@yahoo.com>
2022-11-02 02:40:42 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,16 @@
 
 /**
  * _puts_recursion - Prints a string followed by a new line
- * @s: string
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s)
 	{
-		_putchar('\n');
-		return;
-	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
+
