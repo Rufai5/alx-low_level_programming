71c7949136e1ebc3f58b6b78c10a1e3b47ccd46d
Rufai5 <muhammedrufai@yahoo.com>
2022-11-04 12:20:35 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  writes program name
+ * @argc: arguement count
+ * @argv: array of pointers to CLI arguement
+ *
+ * Return: This return to 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
