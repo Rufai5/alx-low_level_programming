303149575a78a49912eca0170b0a616172ecbb92
Rufai5 <muhammedrufai@yahoo.com>
2022-11-10 07:23:38 -0800

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
+/**
+ * find_len - Finds the length of a string
+ * @str: The string to be measured
+ *
+ * Return: The length of the string
+ */
+int find_len(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * create_xarray - Creates an array of chars and initializes it with
+ * the character 'x'. Adds a terminating null byte
+ * @size: The size of the array to be initialized
+ * Description: If there is insufficient space, the
+ * function exits with a status of 98
+ *
+ * Return: A pointer to the array
+ */
+char *create_xarray(int size)
+{
+	char *array;
+	int index;
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		exit(98);
+
+	for (index = 0; index < (size - 1); index++)
+		array[index] = 'x';
+
+	array[index] = '\0';
+
+	return (array);
+}
+
+/**
+ * get_digit - Converts a digit character to a corresponding int
+ * @c: The character to be converted
+ * Description: If c is a non-digit, the function
+ * exits with a status of 98
+ * Return: The converted int.
+ */
+int get_digit(char c)
+{
+	int digit = c - '0';
+
+	if (digit < 0 || digit > 9)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (digit);
+}
