f0ad4b672a963a8e13403fbcdb7a6ed353dcc1ca
Rufai5 <muhammedrufai@yahoo.com>
2022-11-11 18:46:27 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,71 +1,99 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
+#include "main.h"
 
 /**
- * find_len - Finds the length of a string
- * @str: The string to be measured
+ * is_digit - checks if a string contains a non-digit char
+ * @s :string to be evaluated
  *
- * Return: The length of the string
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int find_len(char *str)
+int is_digit(char *s)
 {
-	int len = 0;
+	int i = 0;
 
-	while (*str++)
-		len++;
-
-	return (len);
-}
-
-/**
- * create_xarray - Creates an array of chars and initializes it with
- * the character 'x'. Adds a terminating null byte
- * @size: The size of the array to be initialized
- * Description: If there is insufficient space, the
- * function exits with a status of 98
- *
- * Return: A pointer to the array
- */
-char *create_xarray(int size)
-{
-	char *array;
-	int index;
-
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-		exit(98);
-
-	for (index = 0; index < (size - 1); index++)
-		array[index] = 'x';
-
-	array[index] = '\0';
-
-	return (array);
-}
-
-/**
- * get_digit - Converts a digit character to a corresponding int
- * @c: The character to be converted
- * Description: If c is a non-digit, the function
- * exits with a status of 98
- * Return: The converted int.
- */
-int get_digit(char c)
-{
-	int digit = c - '0';
-
-	if (digit < 0 || digit > 9)
+	while (s[i])
 	{
-		printf("Error\n");
-		exit(98);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
-	return (digit);
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
