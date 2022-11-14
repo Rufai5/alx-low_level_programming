f4c61352bb6527aa3fe818e8e6c1879e36b3bbcd
Rufai5 <muhammedrufai@yahoo.com>
2022-11-14 10:19:06 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,16 +8,12 @@
  * @owner: owner
  * Description: This struct is for dog
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
-	else
+	if (d != NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
